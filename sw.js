7bd02d963d920b107692ae690906b57409acc52f
w227449 <227449119@qq.com>
2024-06-18 22:11:36 +0800

    Site updated: 2024-06-18 22:11:36

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1718719513410";
+const VERSION = "1718719894056";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
